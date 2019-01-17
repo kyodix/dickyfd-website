7a79bc828a135aa6b4b34c7ea2a496480b66a919
Dicky Fajar D <kyodix@gmail.com>
2019-01-17 08:46:16 +0700

    update 2

--- content/application.php
+++ content/application.php
@@ -1,67 +1,67 @@
 <div class="isiwork">
-    <a href="index.php?page=my_work"><img src="img/folder design.png" alt=""></a>
+    <a href="index.php?halaman=my_work"><img src="img/folder design.png" alt=""></a>
     <img src="img/application icon.png" alt="">
     <h2>APPLICATION</h2>
     <?php
         include 'koneksi.php';
 
-        if(isset($_GET['page'])) {
-            $page = $_GET['page'];
+        if(isset($_GET['halaman'])) {
+            $halaman = $_GET['halaman'];
         } else {
-            $page = 1;
+            $halaman = 1;
         }
 
-        if($page == '' || $page == 1) {
-            $page1 = 0;
+        if($halaman == '' || $halaman == 1) {
+            $halaman1 = 0;
         } else {
-            $page1 = ($page*5)-5;
+            $halaman1 = ($halaman*5)-5;
         }
 
-        $sql = 'SELECT * FROM APPLICATION ORDER BY ID ASC LIMIT '.$page1.', 5';
+        $sql = 'SELECT * FROM APPLICATION ORDER BY ID ASC LIMIT '.$halaman1.', 5';
         $data = $koneksi->query($sql);
 
         // print_r($data->fetch_all());
         while($row = $data->fetch_assoc()) {
             echo '<img src="'.$row['gambarapplication'].'">';
-            echo '<p> Name |'.$row['namaapplication'].'</p>';
-            echo '<p> Platform |'.$row['platformapplication'].'</p>';
-            echo '<p> Software |'.$row['softapplication'].'</p>';
-            echo '<p> Date |'.$row['dateapplication'].'</p>';
-            echo '<p> Category |'.$row['kategoriapplication'].'</p>';
-            echo '<p> Link |'.$row['linkapplication'].'</p>';
+            echo '<p><b>Name</b> | '.$row['namaapplication'].'</p>';
+            echo '<p><b>Platform</b> | '.$row['platformapplication'].'</p>';
+            echo '<p><b>Software</b> | '.$row['softapplication'].'</p>';
+            echo '<p><b>Date</b> | '.$row['dateapplication'].'</p>';
+            echo '<p><b>Category</b> | '.$row['kategoriapplication'].'</p>';
+            echo '<p><b>Link</b> | <i><u><a target="_blank" href="'.$row['linkapplication'].'">'.$row['namaapplication'].'</a></u></i></p>';
         }
 
         //pagination
         $sql = 'SELECT * FROM application';
         $data = $koneksi->query($sql);
         $records = $data->num_rows;
-        $records_pages = $records/5;
-        $records_pages = ceil($records_pages);
-        $prev = $page - 1;
-        $next = $page + 1;
+        $records_halamans = $records/5;
+        $records_halamans = ceil($records_halamans);
+        $prev = $halaman - 1;
+        $next = $halaman + 1;
 
         echo '<ul class="pagination">';
 
-        if($page >= 5) {
-            echo '<li><a href="?page=1">First</a></li>';
+        if($halaman >= 5) {
+            echo '<li><a href="?halaman=1">First</a></li>';
         }
 
         if ($prev >= 1) {
-            echo '<li><a href="?page='.$prev.'">Prev</a></li>';
+            echo '<li><a href="?halaman='.$prev.'">Prev</a></li>';
         }
 
-        if($records_pages >= 2) {
-            for($r=1; $r <= $records_pages; $r++) {
-                $active = $r == $page ? 'class="active"' : '';
-                echo '<li><a href="?page='.$r.'" '.$active.'>'.$r.'</a></li>';
+        if($records_halamans >= 2) {
+            for($r=1; $r <= $records_halamans; $r++) {
+                $active = $r == $halaman ? 'class="active"' : '';
+                echo '<li><a href="?halaman='.$r.'" '.$active.'>'.$r.'</a></li>';
             }
         }
 
-        if ($next <= $records_pages && $records_pages >= 2) {
-            echo '<li><a href="?page='.$next.'">Next</a></li>';
+        if ($next <= $records_halamans && $records_halamans >= 2) {
+            echo '<li><a href="?halaman='.$next.'">Next</a></li>';
         }
-        if($page != $records_pages && $records_pages >=5) {
-            echo '<li><a href="?page='.$records_pages.'">Last</a></li>';
+        if($halaman != $records_halamans && $records_halamans >=5) {
+            echo '<li><a href="?halaman='.$records_halamans.'">Last</a></li>';
         }
 
         echo '</ul>';
